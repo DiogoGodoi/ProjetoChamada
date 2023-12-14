e82cad5b874ff3c903f7825b0d797f86728fe18f
Diogo <diogogoddoi@gmail.com>
2023-12-14 10:24:15 -0300

    Atualização

--- AuthTeste/Controllers/AuthController.cs
+++ AuthTeste/Controllers/AuthController.cs
@@ -11,14 +11,11 @@ namespace AuthTeste.Controllers
 
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly SmtpConfig _smtpConfig;
-        private string tokenReset { get; set; } = "";
 
-        public AuthController(UserManager<IdentityUser> _userManager, SignInManager<IdentityUser> _signInManager, SmtpConfig _smtpConfig)
+        public AuthController(UserManager<IdentityUser> _userManager, SignInManager<IdentityUser> _signInManager)
         {
             this._userManager = _userManager;
             this._signInManager = _signInManager;
-            this._smtpConfig = _smtpConfig;
         }
 
         [HttpGet]
@@ -64,107 +61,6 @@ namespace AuthTeste.Controllers
             ModelState.AddModelError("", "Erro na autenticação");
             return View(user);
         }
-
-		[HttpGet]
-		public IActionResult ForgotPassword()
-        {
-            return View();
-        }
-
-		[HttpPost]
-        [AllowAnonymous]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> ForgotPassword(MdlForgotPassword forgotUser)
-		{
-
-			if (ModelState.IsValid)
-			{
-				var findUser = await _userManager.FindByNameAsync(forgotUser.email);
-
-				if (findUser != null)
-				    {
-					    string code = await _userManager.GeneratePasswordResetTokenAsync(findUser);
-					    var callbackUrl = Url.Action("ResetPassword", "Auth", new { userId = forgotUser.id }, protocol: HttpContext.Request.Scheme);
-
-                        _smtpConfig.corpo = $"Por favor, redefina sua senha clicando <a href='{callbackUrl}'>aqui</a>";
-
-					    _smtpConfig.EnviarEmail(forgotUser.email);
-
-                        TempData["TokenGerado"] = code;
-					    TempData["Email"] = forgotUser.email;
-					    ViewBag.Sucesso = "Link enviado";
-
-					    return View();
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("", "Erro na recuperação da senha");
-                        return View(forgotUser);
-                    }
-            }
-            else
-            {
-                ModelState.AddModelError("", "Erro na recuperação da senha");
-				return View(forgotUser);
-            }
-		}
-
-        [HttpGet]
-        public IActionResult ResetPassword()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ResetPassword(MdlResetPassword resetPassword)
-        {
-
-			if (ModelState.IsValid)
-            {
-                var userFind = await _userManager.FindByEmailAsync(resetPassword.email);
-
-                if (userFind != null)
-                {
-
-                    if(resetPassword.password == resetPassword.confirmPassword)
-                    {
-                        var resultado = await _userManager.ResetPasswordAsync(userFind, resetPassword.token, resetPassword.confirmPassword);
-
-                        if (resultado.Succeeded)
-                        {
-
-                            await _signInManager.RefreshSignInAsync(userFind);
-
-                            ViewBag.Sucesso = "Alterado com sucesso";
-						    return View();
-                        }
-                        else
-                        {
-                            ModelState.AddModelError("", "Erro na redefinição");
-                            return View(resetPassword);
-                        }
-                    }
-                    else
-                    {
-						ModelState.AddModelError("", "Erro na redefinição");
-						return View(resetPassword);
-					}
-                }
-                else
-                {
-					ModelState.AddModelError("", "Erro na redefinição");
-					return View(resetPassword);
-				}
-            }
-            else
-            {
-				ModelState.AddModelError("", "Erro na redefinição");
-				return View(resetPassword);
-            }
-
-		}
 
         [HttpPost]
         [ValidateAntiForgeryToken]
