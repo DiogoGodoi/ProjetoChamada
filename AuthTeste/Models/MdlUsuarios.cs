70c2a66fb019242ab41c2904ed6dee5ee1bdc5bb
Diogo <diogogoddoi@gmail.com>
2023-12-19 13:47:08 -0300

    Atualização

---
+++ AuthTeste/Models/MdlUsuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthTeste.Models
+{
+	public class MdlUsuarios
+	{
+		    public string Id { get; set; }
+			public string? Email { get; set; }
+			public string? UserName { get; set; }
+			public IEnumerable<string>? Roles { get; set; }
+	}
+}
