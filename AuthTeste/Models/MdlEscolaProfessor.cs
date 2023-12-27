e91fd0ee4bf5fbefe821718cc8804b832e5cc33d
Diogo <diogogoddoi@gmail.com>
2023-12-27 10:01:05 -0300

    Atualização

--- AuthTeste/Models/MdlEscolaProfessor.cs
+++ AuthTeste/Models/MdlEscolaProfessor.cs
@@ -9,12 +9,9 @@ namespace AuthTeste.Models
 	{
 		[ForeignKey("Professor")]
 		public int Fk_Professor_Id { get; set; }
-
 		[ForeignKey("Escola")]
 		public int Fk_Escola_Id { get; set; }
-
 		public virtual MdlProfessor? Professor { get; set; }
-
 		public virtual MdlEscola? Escola { get; set; }
 	}
 }
