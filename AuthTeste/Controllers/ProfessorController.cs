1790cb7e13a4230c388c6bb927c7b52de5c1479d
Diogo <diogogoddoi@gmail.com>
2023-12-28 11:12:57 -0300

    Atualização

---
+++ AuthTeste/Controllers/ProfessorController.cs
@@ -0,0 +1,23 @@
+﻿using AuthTeste.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthTeste.Controllers
+{
+	public class ProfessorController : Controller
+	{
+		private readonly IProfessorRepository _professorRepository;
+
+		public ProfessorController(IProfessorRepository _professorRepository) { 
+
+			this._professorRepository = _professorRepository;
+		}
+
+		[HttpGet]
+		public IActionResult ListProfessores()
+		{
+			var professores = _professorRepository.GetProfessores();
+
+			return View(professores);
+		}
+	}
+}
