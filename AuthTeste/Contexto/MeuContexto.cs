d495d1b36cdc6b70abe2632e452fd44fab24c105
Diogo <diogogoddoi@gmail.com>
2024-01-10 10:40:39 -0300

    Atualização

--- AuthTeste/Contexto/MeuContexto.cs
+++ AuthTeste/Contexto/MeuContexto.cs
@@ -7,26 +7,23 @@ namespace AuthTeste.Contexto
 {
     public class MeuContexto: IdentityDbContext<IdentityUser>
     {
-
         public MeuContexto(DbContextOptions<MeuContexto> options): base(options) { }
-
         public DbSet<MdlEscola> Escola { get; set; }    
         public DbSet<MdlProfessor> Professor { get; set;}
         public DbSet<MdlTurma> Turma { get; set; }  
         public DbSet<MdlEscolaProfessor> Escola_Professor { get; set; }
         public DbSet<MdlProfessorTurma> Professor_Turma { get; set; }
-
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
             IConfiguration configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-				.AddJsonFile("appsettings.json")
-				.Build();
+                .AddJsonFile("appsettings.json")
+                .Build();
 
             var defaultConnection = configuration.GetConnectionString("defaultConnection");
 
 			optionsBuilder.UseSqlServer(defaultConnection);
-		}
 
+		}
 	}
 }
